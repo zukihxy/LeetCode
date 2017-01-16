843bcefa5b05aec56f952a7a6ea3200db6cdf011
zukihxy <14302010050@fudan.edu.cn>
2017-01-16 19:08:08 +0800

    two sum 2

---
+++ A167.java
@@ -0,0 +1,17 @@
+package LeetCode;
+
+public class A167 {
+    public int[] twoSum(int[] numbers, int target) {
+    	// 2ms, can it be better?
+        int num = numbers[0]+numbers[numbers.length];
+        int[] result = {1,numbers.length};
+        while (num != target) {
+            if (num > target)
+                result[1]--;
+            else
+                result[0]++;
+            num = numbers[result[0]-1]+numbers[result[1]-1];
+        }
+        return result;
+    }
+}
