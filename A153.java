a4842083a9c6b6481b3447b153f24fb65fc52f86
zukihxy <14302010050@fudan.edu.cn>
2017-01-19 15:32:04 +0800

    find min in roated sorted array

---
+++ A153.java
@@ -0,0 +1,22 @@
+package LeetCode;
+
+public class A153 {
+    public int findMin(int[] num) { // typical binary search
+        int start=0,end=num.length-1;
+        
+        while (start<end) {
+            if (num[start]<num[end])
+                return num[start];
+            
+            int mid = (start+end)/2;
+            
+            if (num[mid]>=num[start]) {
+                start = mid+1;
+            } else {
+                end = mid;
+            }
+        }
+        
+        return num[start];
+    }
+}
---
+++ A199.java
@@ -0,0 +1,35 @@
+package LeetCode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class A199 {
+	public class TreeNode {
+		int val;
+		TreeNode left;
+		TreeNode right;
+
+		TreeNode(int x) {
+			val = x;
+		}
+	}
+
+	public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> result = new ArrayList<Integer>();
+        rightView(root, result, 0);
+        return result;
+    }
+    
+    public void rightView(TreeNode curr, List<Integer> result, int currDepth){
+        if(curr == null){
+            return;
+        }
+        if(currDepth == result.size()){
+            result.add(curr.val);
+        }
+        
+        rightView(curr.right, result, currDepth + 1);
+        rightView(curr.left, result, currDepth + 1);
+        
+    }
+}
