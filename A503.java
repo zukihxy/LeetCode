8b2e51f62eb07178f0e4bac4eae6ae58d7644fd1
zukihxy <14302010050@fudan.edu.cn>
2017-02-14 09:44:15 +0800

    rotten days 503

---
+++ A503.java
@@ -0,0 +1,19 @@
+package LeetCode;
+
+import java.util.Arrays;
+import java.util.Stack;
+
+public class A503 {
+	public int[] nextGreaterElements(int[] nums) {
+        int n = nums.length, next[] = new int[n];
+        Arrays.fill(next, -1);
+        Stack<Integer> stack = new Stack<>(); // index stack
+        for (int i = 0; i < n * 2; i++) {
+            int num = nums[i % n]; 
+            while (!stack.isEmpty() && nums[stack.peek()] < num)
+                next[stack.pop()] = num;
+            if (i < n) stack.push(i);
+        }   
+        return next;
+    }
+}
